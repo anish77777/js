67c4288b1ed4ab01655eabfd568afa3942f71efe
anish <85100574+anish77777@users.noreply.github.com>
2023-12-10 02:24:46 +0000

    conversion of datatype

---
+++ 03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33abc"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+//"33"=33
+//"33abc"=>NaN
+// true => 1; false => 0
+
+let isloggedin= ""
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin)
+//1 =>true,0 => false
+// "hitesh" => true
+//""= false
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
